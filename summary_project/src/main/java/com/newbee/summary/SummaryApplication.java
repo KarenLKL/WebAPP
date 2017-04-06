18882759d6cbae755fa718a523a92adb25da8567
likailun <likailun@gznb.com>
2017-04-06 15:32:08 +0800

    测试mybatis配置是否正确

--- summary_project/src/main/java/com/newbee/summary/SummaryApplication.java
+++ summary_project/src/main/java/com/newbee/summary/SummaryApplication.java
@@ -1,7 +1,11 @@
 package com.newbee.summary;
 
+import com.github.pagehelper.PageHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class SummaryApplication {
@@ -9,4 +13,5 @@ public class SummaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SummaryApplication.class, args);
 	}
+
 }
